391a101fb731e2c670675f3466c9ea116472af50
Meredith Grimes <meredithagrimes@gmail.com>
2019-11-12 00:05:37 -0500

    Updated index.html and questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+let questions = [
+
+    {
+
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+
+    },{
+
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+
+    },{
+
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+
+    }
+
+];
