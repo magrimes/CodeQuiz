ee3a226d06400ff6ebf005971e7a2caa8c792063
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-20 18:33:51 -0400

    created variables

---
+++ script.js
@@ -0,0 +1,53 @@
+var startButton = document.getElementById("startQuiz");
+var quizRules = document.getElementById("quizRules");
+var quizBox = document.getElementById("quizBox");
+var quizQuestions = document.getElementById("question");
+var quizChoices = document.getElementById("choices");
+var answerBox = document.getElementById("ansMessage");
+var answer = document.getElementById("listAns");
+var completed = document.getElementById("completedMessage");
+var finalScoreMessage = document.getElementById("finalScoreMessage");
+var playerName = document.getElementById("playerName");
+
+var playerName = document.querySelector("#nameField");
+var submitButton = document.querySelector("#submitButton");
+var highScoreContainer = document.querySelector("#highScoreContainer");
+var highScoreHeader = document.querySelector("#highScoreHeader");
+var highScoreList = document.querySelector("#highScoreList");
+var displayHS = document.querySelector("#displayHS");
+var highScoreButton = document.querySelector("#highScoreButton");
+var completedQuiz = document.querySelector("#completedContainer");
+var timerE1 = document.querySelector(".timer");
+
+var scores = [];
+var setTimer = 0;
+var finalScore = 0;
+var i = 0;
+var savedScores = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
